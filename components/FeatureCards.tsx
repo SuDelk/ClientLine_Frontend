886acbb59e084c3db38b7fd6a847e1ca27c401f8
Ranjula001 <ranjulailukpitiya@gmail.com>
2025-12-16 11:49:30 +0530

    react icons

--- components/FeatureCards.tsx
+++ components/FeatureCards.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useTheme } from '@/contexts/ThemeContext';
+import { IoAnalytics } from 'react-icons/io5';
+import { RxCalendar } from 'react-icons/rx';
 
 const FeatureCards: React.FC = () => {
   const { theme } = useTheme();
@@ -15,9 +17,7 @@ const FeatureCards: React.FC = () => {
       {/* Performance Tracking Card */}
       <div className={cardBaseClass}>
         <div className="flex items-center justify-center w-12 h-12 bg-[#17A2B8] rounded-full mb-4">
-          <svg className="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
-          </svg>
+          <IoAnalytics className="w-6 h-6 text-white" />
         </div>
         
         <h3 className={`text-xl font-semibold mb-2 ${
@@ -38,9 +38,7 @@ const FeatureCards: React.FC = () => {
       {/* Handle Appointments Card */}
       <div className={cardBaseClass}>
         <div className="flex items-center justify-center w-12 h-12 bg-[#17A2B8] rounded-full mb-4">
-          <svg className="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-          </svg>
+          <RxCalendar className="w-6 h-6 text-white" />
         </div>
         
         <h3 className={`text-xl font-semibold mb-2 ${
